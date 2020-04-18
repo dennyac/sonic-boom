0ffad32eca21707f78f5a502a5800044673a09c8
Denny Abraham Cheriyan <dennyac@gmail.com>
2020-04-19 00:31:37 +0530

    Add lead section

--- smile_meditation.rb
+++ smile_meditation.rb
@@ -23,6 +23,9 @@ bass_settings = {
   delayed_release: { attack: 0, sustain: 0.3, release: 0.2, amp: 0.57 }
 }
 
+flute_settings = { attack: 0.05, sustain: 0.3, release: 0.15, amp: 0.48 }
+guitar_settings = { amp: 0.28, attack:  0.007,   sustain: 0.3, release: 1 }
+
 define :intro do
   i = 0.2
   6.times do
@@ -39,7 +42,7 @@ end
 
 define :verse do
   cue :start_bass_and_drums
-  2.times do
+  5.times do
     4.times do
       play [:G3, :AS3, :D4], keyboard_settings[:regular]
       sleep 0.7
@@ -118,7 +121,7 @@ define :bridge do
     sleep 0.7
   end
   4.times do
-    play [:B3, :D4, :F4, :G4], keyboard_settings[:regular]
+    play [:B3, :D4, :F4, :G4], keyboard_settings[:quick_release]
     sleep 0.35
   end
   2.times do
@@ -156,7 +159,7 @@ define :bridge do
 end
 
 define :drums do
-  5.times do
+  11.times do
     sample :drum_bass_soft, drum_settings[:bass]
     sample :drum_cymbal_closed, drum_settings[:cymbol]
     sleep 0.35
@@ -388,7 +391,7 @@ define :drums do
 end
 
 define :bass_begin do
-  2.times do
+  5.times do
     play :Eb2, bass_settings[:regular]
     sleep 1.225
     play :Eb2, bass_settings[:quick_release]
@@ -515,6 +518,230 @@ define :bass do
   bass_bridge
 end
 
+
+def get_note_symbol(note, octave)
+  "#{note}#{octave}".to_sym
+end
+
+define :lead_first do |octave = 4, opts = {}|
+  play get_note_symbol("D", octave + 1), opts
+  sleep 0.175
+  play get_note_symbol("DS", octave + 1), opts
+  sleep 0.175
+  play get_note_symbol("D", octave + 1), opts
+  sleep 0.35
+  play get_note_symbol("AS", octave), opts
+  sleep 0.35
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  note = play get_note_symbol("F", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("G", octave)
+  sleep 0.875
+  play get_note_symbol("F", octave), opts
+  sleep 0.7
+  note = play get_note_symbol("C", octave), opts.merge(note_slide: 0.05)
+  sleep 0.35
+  control note, note: get_note_symbol("D", octave)
+  sleep 0.35
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  note = play get_note_symbol("AS", octave - 1), opts.merge(note_slide: 0.01)
+  sleep 0.01
+  control note, note: get_note_symbol("C", octave)
+  sleep 1.04
+  play get_note_symbol("AS", octave - 1), opts
+  sleep 0.7
+  note = play get_note_symbol("C", octave), opts.merge(note_slide: 0.1)
+  sleep 0.1
+  control note, note: get_note_symbol("D", octave)
+  sleep 0.3
+  play get_note_symbol("AS", octave - 1), opts
+  sleep 0.6
+  note = play get_note_symbol("F", octave), opts.merge(note_slide: 0.1)
+  sleep 0.2
+  control note, note: get_note_symbol("G", octave)
+  sleep 4.025
+end
+
+define :lead_mid do |octave = 4, opts = {}|
+  play get_note_symbol("D", octave + 1), opts
+  sleep 0.175
+  play get_note_symbol("DS", octave + 1), opts
+  sleep 0.175
+  play get_note_symbol("D", octave + 1), opts
+  sleep 0.35
+  play get_note_symbol("AS", octave), opts
+  sleep 0.35
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  note = play get_note_symbol("F", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("G", octave)
+  sleep 0.875
+  play get_note_symbol("F", octave), opts
+  sleep 0.7
+  note = play get_note_symbol("C", octave), opts.merge(note_slide: 0.05)
+  sleep 0.0875
+  control note, note: get_note_symbol("D", octave)
+  sleep 0.2625
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  play get_note_symbol("G", octave), opts
+  sleep 0.35
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  note = play get_note_symbol("G", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("GS", octave)
+  sleep 0.525
+  play get_note_symbol("G", octave), opts
+  sleep 0.7
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  play get_note_symbol("G", octave), opts
+  sleep 0.7
+  note = play get_note_symbol("C", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("D", octave)
+  sleep 0.525
+  note = play get_note_symbol("F", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("G", octave)
+  sleep 0.175
+  note = play get_note_symbol("C", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("D", octave)
+  sleep 2.975
+end
+
+define :lead_last do |octave = 4, opts = {}|
+  play get_note_symbol("D", octave + 1), opts
+  sleep 0.175
+  play get_note_symbol("DS", octave + 1), opts
+  sleep 0.175
+  play get_note_symbol("D", octave + 1), opts
+  sleep 0.35
+  play get_note_symbol("AS", octave), opts
+  sleep 0.35
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  note = play get_note_symbol("F", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("G", octave)
+  sleep 0.875
+  play get_note_symbol("F", octave), opts
+  sleep 0.7
+  note = play get_note_symbol("C", octave), opts.merge(note_slide: 0.05)
+  sleep 0.0875
+  control note, note: get_note_symbol("D", octave)
+  sleep 0.2625
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  play get_note_symbol("G", octave), opts
+  sleep 0.35
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  note = play get_note_symbol("G", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("GS", octave)
+  sleep 0.525
+  play get_note_symbol("G", octave), opts
+  sleep 0.7
+  play get_note_symbol("F", octave), opts
+  sleep 0.35
+  play get_note_symbol("DS", octave), opts
+  sleep 0.7
+  note = play get_note_symbol("C", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("D", octave)
+  sleep 0.525
+  play get_note_symbol("G", octave), opts
+  sleep 1.75
+end
+
+define :lead_bridge do |octave = 4, opts = {}|
+  play get_note_symbol("G", octave), opts
+  sleep 0.175
+  play get_note_symbol("AS", octave), opts
+  sleep 0.175
+  play get_note_symbol("C", octave + 1), opts
+  sleep 0.35
+  note = play get_note_symbol("C", octave + 1), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("D", octave + 1)
+  sleep 0.525
+  play get_note_symbol("AS", octave), opts
+  sleep 0.7
+  play get_note_symbol("F", octave), opts
+  sleep 0.7
+  play get_note_symbol("DS", octave), opts
+  sleep 0.35
+  note = play get_note_symbol("C", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("D", octave)
+  sleep 2.275
+  play get_note_symbol("G", octave), opts
+  sleep 0.175
+  play get_note_symbol("AS", octave), opts
+  sleep 0.175
+  play get_note_symbol("C", octave + 1), opts
+  sleep 0.35
+  note = play get_note_symbol("C", octave + 1), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("D", octave + 1)
+  sleep 0.525
+  play get_note_symbol("AS", octave), opts
+  sleep 0.7
+  play get_note_symbol("F", octave), opts
+  sleep 0.7
+  note = play get_note_symbol("F", octave + 1), opts.merge(note_slide: 0.02)
+  sleep 0.1125
+  control note, note: get_note_symbol("G", octave + 1)
+  sleep 0.2375
+  note = play get_note_symbol("F", octave + 1), opts.merge(note_slide: 0.02)
+  sleep 0.1125
+  control note, note: get_note_symbol("G", octave + 1)
+  sleep 1.9875
+  play get_note_symbol("F", octave + 1), opts
+  sleep 0.35
+  play get_note_symbol("D", octave + 1), opts
+  sleep 0.175
+  play get_note_symbol("DS", octave + 1), opts
+  sleep 0.175
+  play get_note_symbol("F", octave + 1), opts
+  sleep 0.35
+  note = play get_note_symbol("C", octave + 1), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("D", octave + 1)
+  sleep 0.525
+  play get_note_symbol("AS", octave), opts
+  sleep 0.7
+  play get_note_symbol("F", octave), opts
+  sleep 0.7
+  play get_note_symbol("DS", octave), opts
+  sleep 0.35
+  note = play get_note_symbol("C", octave), opts.merge(note_slide: 0.05)
+  sleep 0.175
+  control note, note: get_note_symbol("D", octave)
+  sleep 0.875
+  play get_note_symbol("AS", octave), opts
+  sleep 0.7
+  play get_note_symbol("C", octave + 1), opts
+  sleep 0.7
+  play get_note_symbol("D", octave + 1), opts.merge(release: 4)
+  sleep 4.9
+end
+
+define :lead do  |octave = 4, opts = {}, synth = :beep|
+  use_synth synth
+  lead_first octave, opts
+  lead_mid octave, opts
+  lead_first octave, opts
+  lead_last octave, opts
+  lead_bridge octave, opts
+end
+
 in_thread(name: :keyboard) do
   use_synth :beep
   intro
@@ -530,8 +757,25 @@ in_thread(name: :drums) do
     drums
   end
 end
+in_thread(name: :lead_flute) do
+  sync :start_bass_and_drums
+  sleep_time = 23.45
+  2.times do
+    sleep sleep_time
+    lead 5, flute_settings
+    sleep_time += 1.4
+  end
+end
+in_thread(name: :lead_guitar) do
+  sync :start_bass_and_drums
+  sleep_time = 23.45
+  2.times do
+    sleep sleep_time
+    lead 4, guitar_settings, :pluck
+    sleep_time += 1.4
+  end
+end
 2.times do
   sync :start_bass_and_drums
   bass
 end
-
